d9f0dc6a0ce3f440a7858824f46f599a398b111b
monkbai <zhiboliu97@gmail.com>
2022-03-22 14:56:55 +0800

    add pin tool

---
+++ MyPinTool/FunCallRdx.cpp
@@ -0,0 +1,226 @@
+/*
+ * Copyright 2002-2019 Intel Corporation.
+ * 
+ * This software is provided to you as Sample Source Code as defined in the accompanying
+ * End User License Agreement for the Intel(R) Software Development Products ("Agreement")
+ * section 1.L.
+ * 
+ * This software and the related documents are provided as is, with no express or implied
+ * warranties, other than those that are expressly stated in the License.
+ */
+
+/*
+ *  This file contains an ISA-portable PIN tool for tracing memory accesses.
+ */
+
+#include <stdio.h>
+#include <unordered_map>
+#include <iostream>
+#include <fstream>
+#include <list>
+#include "pin.H"
+
+static std::unordered_map<ADDRINT, std::string> str_of_ins_at;
+
+FILE * trace;
+
+// just want a hash table
+static std::unordered_map<uint64_t, uint64_t> addrs_list;
+/* ===================================================================== */
+// Command line switches
+/* ===================================================================== */
+KNOB<std::string> KnobOutputFile(KNOB_MODE_WRITEONCE,  "pintool",
+    "o", "", "specify file name for MyPinTool output");
+
+KNOB<std::string>   KnobAddrsFile(KNOB_MODE_WRITEONCE,  "pintool",
+    "addrs_file", "", "file path");
+    
+
+/* ===================================================================== */
+// Utilities
+/* ===================================================================== */
+
+VOID Dump(VOID * ip, ADDRINT rdi_value, ADDRINT rsi_value, ADDRINT rdx_value, ADDRINT rcx_value){
+    //fprintf(trace, "Start:\n");
+    // get address from register rdi 
+    // void * rdi = (void *)rdi_value;
+    // void * rsi = (void *)rsi_value;
+    uint64_t * rdx = (uint64_t *)rdx_value;
+    // void * rcx = (void *)rcx_value;
+    fprintf(trace,"%p: ", ip);
+    
+    fprintf(trace, "%lu, ", *(rdx+0));
+    fprintf(trace, "%lu, ", *(rdx+1));
+    fprintf(trace, "%lu, ", *(rdx+2));
+    fprintf(trace, "%lu", *(rdx+3));
+    
+    fprintf(trace, "\n");
+    return;
+}
+
+VOID RecordInst(VOID * ip)
+{
+    fprintf(trace,"%p\n", ip);
+    //std::string ins_str = str_of_ins_at[(ADDRINT)ip];
+    //fprintf(trace,"%p:\t%s\n", ip, ins_str.c_str());
+    //fprintf(trace,"N:\t%p:\t%d\n", (void *)0xDEADBEEF, 7); // not real memory op
+    //fprintf(trace,"N:\n");
+}
+
+// Print a memory read record
+VOID RecordMemRead(VOID * ip, VOID * mem_addr, USIZE mem_size)
+{
+    fprintf(trace,"%p\n", ip);
+    //std::string ins_str = str_of_ins_at[(ADDRINT)ip];
+    //fprintf(trace,"%p:\t%s\n", ip, ins_str.c_str());
+    //fprintf(trace,"R:\t%p:\t%lu\n", mem_addr, mem_size);
+    //fprintf(trace,"%p: R %p\n", ip, addr);
+}
+
+// Print a memory write record
+VOID RecordMemWrite(VOID * ip, VOID * mem_addr, USIZE mem_size)
+{
+    fprintf(trace,"%p\n", ip);
+    //std::string ins_str = str_of_ins_at[(ADDRINT)ip];
+    //fprintf(trace,"%p:\t%s\n", ip, ins_str.c_str());
+    //fprintf(trace,"W:\t%p:\t%lu\n", mem_addr, mem_size);
+    //fprintf(trace,"%p: W %p\n", ip, addr);
+}
+
+// Is called for every instruction and instruments reads and writes
+VOID Instruction(INS ins, VOID *v)
+{
+    ADDRINT ins_addr = INS_Address(ins);
+    
+    // Only instrument addrs in the addrs_file 
+    if (addrs_list.find(ins_addr) == addrs_list.end()){
+        return;
+    }
+    
+    str_of_ins_at[INS_Address(ins)] = INS_Disassemble(ins);
+    std::string ins_asm = INS_Disassemble(ins);
+    
+    INS_InsertPredicatedCall(
+        ins, IPOINT_BEFORE, (AFUNPTR)Dump,
+        IARG_INST_PTR,
+        IARG_REG_VALUE, LEVEL_BASE::REG_RDI,
+        IARG_REG_VALUE, LEVEL_BASE::REG_RSI,
+        IARG_REG_VALUE, LEVEL_BASE::REG_RDX,
+        IARG_REG_VALUE, LEVEL_BASE::REG_RCX,
+        IARG_END);
+    return;
+    
+    // Instruments memory accesses using a predicated call, i.e.
+    // the instrumentation is called iff the instruction will actually be executed.
+    //
+    // On the IA-32 and Intel(R) 64 architectures conditional moves and REP 
+    // prefixed instructions appear as predicated instructions in Pin.
+    UINT32 memOperands = INS_MemoryOperandCount(ins);
+    
+    if (memOperands == 0){
+        INS_InsertPredicatedCall(
+            ins, IPOINT_BEFORE, (AFUNPTR)RecordInst,
+            IARG_INST_PTR,
+            IARG_END);
+    }
+
+    // Iterate over each memory operand of the instruction.
+    for (UINT32 memOp = 0; memOp < memOperands; memOp++)
+    {
+        if (INS_MemoryOperandIsRead(ins, memOp))
+        {
+            USIZE mem_size = INS_MemoryReadSize(ins);
+            INS_InsertPredicatedCall(
+                ins, IPOINT_BEFORE, (AFUNPTR)RecordMemRead,
+                IARG_INST_PTR,
+                IARG_MEMORYOP_EA, memOp,
+                IARG_UINT64, mem_size,
+                IARG_END);
+        }
+        // Note that in some architectures a single memory operand can be 
+        // both read and written (for instance incl (%eax) on IA-32)
+        // In that case we instrument it once for read and once for write.
+        if (INS_MemoryOperandIsWritten(ins, memOp))
+        {
+            USIZE mem_size = INS_MemoryWriteSize(ins);
+            INS_InsertPredicatedCall(
+                ins, IPOINT_BEFORE, (AFUNPTR)RecordMemWrite,
+                IARG_INST_PTR,
+                IARG_MEMORYOP_EA, memOp,
+                IARG_UINT64, mem_size,
+                IARG_END);
+        }
+    }
+}
+
+VOID Fini(INT32 code, VOID *v)
+{
+    fprintf(trace, "#eof\n");
+    fclose(trace);
+}
+
+/* ===================================================================== */
+/* Print Help Message                                                    */
+/* ===================================================================== */
+   
+INT32 Usage()
+{
+    PIN_ERROR( "This Pintool prints a trace of memory addresses\n" 
+              + KNOB_BASE::StringKnobSummary() + "\n");
+    return -1;
+}
+
+int ReadAddrList(){
+    std::string addrs_file = KnobAddrsFile.Value();
+    FILE *fp = fopen(addrs_file.c_str(),"r");
+    //int count = 0;
+    while(!feof(fp)){
+        uint64_t current_addr;
+        fscanf(fp, "%lx\n", &current_addr);
+        addrs_list[current_addr] = current_addr;
+        //printf("insert 0x%lx\n", current_addr); // debug
+        //count += 1;
+        //printf("%d\n", count);
+    }
+    return 0;
+}
+
+
+/* ===================================================================== */
+/* Main                                                                  */
+/* ===================================================================== */
+
+int main(int argc, char *argv[])
+{
+    if (PIN_Init(argc, argv)) return Usage();
+
+    std::string fileName = KnobOutputFile.Value();
+    trace = fopen(fileName.c_str(), "w");
+    //trace = fopen("pinatrace.out", "w");
+    
+    ReadAddrList();    
+    
+    // debug
+    //printf("output: %s, start: %p, end: %p\n", fileName.c_str(), (void *)start_addr, (void *)end_addr);
+    
+    /*
+    std::unordered_map<uint64_t, uint64_t>::iterator iter;
+    iter = addrs_list.begin();
+    int count = 0;
+    while(iter != addrs_list.end()) {
+        printf("0x%lx\n", iter->second);
+        iter++;
+        count += 1;
+        printf("%d\n", count);
+    }
+    return 0;
+    */
+    
+    INS_AddInstrumentFunction(Instruction, 0);
+    PIN_AddFiniFunction(Fini, 0);
+
+    // Never returns
+    PIN_StartProgram();
+    
+    return 0;
+}
