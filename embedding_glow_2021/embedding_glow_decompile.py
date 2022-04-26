5d1901e69e9068abfda65fa15564fc5fa25c0650
monkbai <zhiboliu97@gmail.com>
2022-04-26 20:56:18 +0800

    init: decompiler robustness - Glow 2021

---
+++ embedding_glow_2021/embedding_glow_decompile.py
@@ -0,0 +1,132 @@
+#! /usr/bin/python3
+import os
+import sys
+sys.path.append("../")
+import math
+import utils
+from utils import list_to_json, dict_to_json, json_to_list, json_to_dict
+import pin_tools
+
+
+def get_embedding_size(memcpy_plt: str, log_path: str):  # 0x401050 -- embedding is implemented with memcpy
+    log_path = os.path.abspath(log_path)
+    addr_list = [memcpy_plt]
+    pin_tools.fun_call_rdi_rsi(prog_path, in_data, addr_list, log_path)
+    with open(log_path, 'r') as f:
+        lines = f.readlines()
+        line = lines[0]
+        size = line[line.find('rdx'):].split(' ')[1].strip()
+        size = size.strip(', ')
+        size = int(size, 16) / 4
+        src_list = []
+        for line in lines:
+            if not line.startswith('#'):
+                src_addr = line[line.find('rsi')+3:].split(',')[0].strip()
+                src_addr = int(src_addr, 16)
+                src_list.append(src_addr)
+    return size, src_list
+
+
+def get_embedding_region(entry_func: str, next_func: str, src_list: list):
+    entry_path = os.path.join(utils.funcs_dir, entry_func)
+    next_func_path = os.path.join(utils.funcs_dir, next_func)
+
+    start_addr, end_addr = utils.get_func_range(entry_path)
+    addr_list = [start_addr]
+    pin_tools.fun_call_rdi_rsi(prog_path, in_data, addr_list, log_path)
+    with open(log_path, 'r') as f:
+        lines = f.readlines()
+        line = lines[0]
+        region_start = line[line.find('rdi')+3:].split(',')[0].strip()
+        region_start = int(region_start, 16)
+
+    start_addr, end_addr = utils.get_func_range(next_func_path)
+    utils.mem_read_log(mem_read_log_path, start_addr, end_addr, prog_path, in_data)
+    read_mem_regions = utils.memory_slices(mem_read_log_path)
+    read_mem_regions = sorted(read_mem_regions, key=lambda x: x[0])
+    for mem_blk in read_mem_regions:
+        embedding_flag = False
+        for src_addr in src_list:
+            if mem_blk[0] <= src_addr <= mem_blk[1]:
+                embedding_flag = True
+                break
+        if not embedding_flag:
+            region_end = mem_blk[0]
+            break
+    return region_start, region_end
+
+
+if __name__ == '__main__':
+    utils.funcs_dir = "/export/d1/zliudc/DLE_Decompiler/TVM/rebuild_ida/embedding_extra/embedding_glow_2022_funcs/"
+
+    # prepared in advance
+    prog_path = "/export/d1/zliudc/DLE_Decompiler/TVM/rebuild_ida/embedding_extra/embedding_glow_2022"
+    in_data = ''  # no input needed, hard coded in source code
+    label_file = "/export/d1/zliudc/DLE_Decompiler/TVM/rebuild_ida/embedding_extra/embedding_glow_2022_funcs/labels.txt"
+    config_file = './config.json'
+
+    # (tmp files) generated during analysis
+    log_path = './embedding_glow_func_call.log'
+    tmp_log_path = './inst_trace.log'
+    exp_log_path = './mem_exp.log'
+    mem_read_log_path = './mem_read.log'
+    mem_write_log_path = './mem_write.log'
+    mem_dump_log_path = './mem_dump.log'
+
+    # ===============================
+    # Recover the Computational Graph
+    # ===============================
+    utils.get_funcs_trace(prog_path, in_data, log_path, label_file)
+    addr2param = utils.print_layer_label(log_path, config_file)
+    func_meta_data, topo_list = utils.print_input_id(log_path, compiler='glow', addr2param=addr2param)
+    
+    # ===============================
+    # Recover the embedding layer, which does not have a specific function call
+    # ===============================
+    size, src_list = get_embedding_size('0x401050', log_path)  # we can get the address of memcpy
+    emb_start, emb_end = get_embedding_region('0008.embedding_2.txt', '0010.libjit_matmul_f.txt', src_list)  # we can also know next layer with parameters (from funcs call trace)
+    print('embedding dict: start {}, end {}, vector size {}, vocabulary size {}'.
+          format(hex(emb_start), hex(emb_end), size, math.floor((emb_end-emb_start)/4/size)))
+
+    # ===============================
+    # Recover other layers
+    # ===============================
+    func_data = {
+                 'avg_pool': '0009.libjit_avg_pool_f.txt',
+                 'matmul': '0010.libjit_matmul_f.txt',
+                 # 'add': '0011.libjit_stacked_kernel' # no need
+                 }
+
+    for func_type, func_name in func_data.items():
+        utils.generate_inst_trace(func_name, tmp_log_path, prog_path, in_data)
+
+        utils.generate_symbolic_expression(func_name, tmp_log_path, exp_log_path, max_inst=5000000)
+
+        shape = utils.recover_shape(func_name, exp_log_path,
+                                    mem_read_log_path, mem_write_log_path,
+                                    prog_path, in_data, func_type=func_type)
+        for i in range(len(func_meta_data)):
+            if func_meta_data[i][0] == func_name:
+                func_meta_data[i][1] = shape
+                break
+        print(shape)
+    
+    list_to_json(topo_list, './topo_list.json')
+    dict_to_json(func_meta_data, './meta_data.json')
+    # ===============================
+    # Extract Parameters
+    # ===============================
+    # func_meta_data is collected from previous output
+    func_meta_data = [
+                      ('0008.embedding_2.txt', (25002, 100), '0x401270', '0x404080', 'embedding'),  # embedding need to be manually set
+                      ('0010.libjit_matmul_f.txt', (1, 100), '0x401610', '0xd8da80', 'matmul'),
+                      ('0011.libjit_stacked_kernel.txt', (1, 1), '0x401690', '0xd8da40', 'add'),
+                      ]
+    for fun_data in func_meta_data:
+        func_name = fun_data[0]
+        w_shape = fun_data[1]
+        dump_point = fun_data[2]
+        dump_addr = fun_data[3]
+        func_type = fun_data[4]
+        utils.extract_params_general(prog_path, in_data, w_shape, dump_point,
+                                     mem_dump_log_path, func_name, dump_addr)
---
+++ resnet18_glow_2021/resnet18_glow_rebuild.py
@@ -0,0 +1,258 @@
+import torch.nn as nn
+import torch
+import json
+import numpy as np
+import time
+import torch.nn.functional as F
+
+
+def read_json(json_path: str):
+    with open(json_path, 'r') as f:
+        j_txt = f.read()
+        list_obj = json.loads(s=j_txt)
+        arr_obj = np.array(list_obj, dtype=np.float32)
+        tensor_obj = torch.from_numpy(arr_obj)
+        return tensor_obj
+
+
+def set_weights(module: nn.modules, json_path: str):
+    w = read_json(json_path)
+    weight = torch.nn.Parameter(w)
+    module.weight = weight
+    if module.bias is not None:
+        torch.nn.init.zeros_(module.bias)
+
+
+def set_biases(module: nn.modules, json_path: str):
+    w = read_json(json_path)
+    w = w.reshape(w.shape[1])
+    module.bias = torch.nn.Parameter(w)
+
+
+def set_bn_weights(module: nn.modules, json_path: str):
+    w = read_json(json_path)
+    w = w.reshape(w.shape[1])
+    module.weight = torch.nn.Parameter(w)
+    module.training = False
+
+
+def set_mean(module: nn.modules, json_path: str):
+    w = read_json(json_path)
+    w = w.reshape(w.shape[1])
+    module.running_mean = w  # torch.nn.Parameter(w)
+    #w = torch.nn.Parameter(w)
+    #module.running_mean = w
+
+
+def set_var(module: nn.modules, json_path: str):
+    w = read_json(json_path)
+    w = w.reshape(w.shape[1])
+    module.running_var = w  # torch.nn.Parameter(w)
+    #w = torch.nn.Parameter(w)
+    #module.running_var = w
+
+
+class MyResNet(nn.Module):
+    def __init__(self):
+        super(MyResNet, self).__init__()
+        net = []
+        net.append(nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3))
+        set_weights(net[-1], './0010.weights_0.json')
+        set_biases(net[-1], '0010.biases_0.json')
+        net.append(nn.ReLU())
+
+        net.append(nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+
+        net.append(nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1))
+        set_weights(net[-1], './0012.weights_0.json')
+        set_biases(net[-1], '0012.biases_0.json')
+        net.append(nn.ReLU())
+
+        net.append(nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1))
+        set_weights(net[-1], './0013.weights_0.json')
+        set_biases(net[-1], '0013.biases_0.json')
+        net.append(nn.Identity())  # net.append(nn.ReLU())
+
+        net.append(nn.ReLU())  # add relu
+
+        net.append(nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1))
+        # set_weights(net[-1], '../resnet18_tvm_O0/0080.function_409280.weights_2.json')
+        set_weights(net[-1], './0012.weights_1.json')
+        set_biases(net[-1], './0012.biases_1.json')
+        net.append(nn.ReLU())
+
+        net.append(nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1))
+        set_weights(net[-1], './0013.weights_1.json')
+        set_biases(net[-1], '0013.biases_1.json')
+        net.append(nn.Identity())  # net.append(nn.ReLU())
+
+        net.append(nn.ReLU())  # add relu
+
+        net.append(nn.Conv2d(in_channels=64, out_channels=128, kernel_size=1, stride=2, padding=0))
+        set_weights(net[-1], './0018.weights_0.json')
+        set_biases(net[-1], '0018.biases_0.json')
+        net.append(nn.Identity())  # net.append(nn.ReLU())
+
+        net.append(nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1))  # correct the shape
+        set_weights(net[-1], './0019.weights_0.json')
+        set_biases(net[-1], '0019.biases_0.json')
+        net.append(nn.ReLU())
+
+        net.append(nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1))
+        set_weights(net[-1], './0020.weights_0.json')
+        set_biases(net[-1], '0020.biases_0.json')
+        net.append(nn.Identity())  # net.append(nn.ReLU())
+
+        net.append(nn.ReLU())  # add relu
+
+        net.append(nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1))
+        set_weights(net[-1], './0023.weights_0.json')
+        set_biases(net[-1], '0023.biases_0.json')
+        net.append(nn.ReLU())
+
+        net.append(nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1))
+        set_weights(net[-1], './0020.weights_1.json')
+        set_biases(net[-1], '0020.biases_1.json')
+        net.append(nn.Identity())  # net.append(nn.ReLU())
+
+        net.append(nn.ReLU())  # add relu
+
+        net.append(nn.Conv2d(in_channels=128, out_channels=256, kernel_size=1, stride=2, padding=0))
+        set_weights(net[-1], './0026.weights_0.json')
+        set_biases(net[-1], '0026.biases_0.json')
+        net.append(nn.Identity())  # net.append(nn.ReLU())
+
+        net.append(nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1))
+        set_weights(net[-1], './0027.weights_0.json')
+        set_biases(net[-1], '0027.biases_0.json')
+        net.append(nn.ReLU())
+
+        net.append(nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1))
+        set_weights(net[-1], './0028.weights_0.json')
+        set_biases(net[-1], '0028.biases_0.json')
+        net.append(nn.Identity())  # net.append(nn.ReLU())
+
+        net.append(nn.ReLU())  # add relu
+
+        net.append(nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1))
+        set_weights(net[-1], './0031.weights_0.json')
+        set_biases(net[-1], '0031.biases_0.json')
+        net.append(nn.ReLU())
+
+        net.append(nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1))
+        set_weights(net[-1], './0028.weights_1.json')
+        set_biases(net[-1], '0028.biases_1.json')
+        net.append(nn.Identity())  # net.append(nn.ReLU())
+
+        net.append(nn.ReLU())  # add relu
+
+        net.append(nn.Conv2d(in_channels=256, out_channels=512, kernel_size=1, stride=2, padding=0))
+        set_weights(net[-1], './0034.weights_0.json')
+        set_biases(net[-1], '0034.biases_0.json')
+        net.append(nn.Identity())  #  net.append(nn.ReLU())
+
+        net.append(nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=2, padding=1))
+        set_weights(net[-1], './0035.weights_0.json')
+        set_biases(net[-1], '0035.biases_0.json')
+        net.append(nn.ReLU())
+
+        net.append(nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1))
+        set_weights(net[-1], './0036.weights_0.json')
+        set_biases(net[-1], '0036.biases_0.json')
+        net.append(nn.Identity())  # net.append(nn.ReLU())
+
+        net.append(nn.ReLU())  # add relu
+
+        net.append(nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1))
+        set_weights(net[-1], './0039.weights_0.json')
+        set_biases(net[-1], '0039.biases_0.json')
+        net.append(nn.ReLU())
+
+        net.append(nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1))
+        set_weights(net[-1], './0036.weights_1.json')
+        set_biases(net[-1], '0036.biases_1.json')
+        net.append(nn.Identity())  # net.append(nn.ReLU())
+
+        net.append(nn.ReLU())  # add relu
+
+        net.append(nn.AvgPool2d(kernel_size=7, stride=1))
+        net.append(nn.Flatten())
+
+        net.append(nn.Linear(in_features=512, out_features=1000))
+        set_weights(net[-1], './0043.params_0.json')
+        set_biases(net[-1], './0043.biases_0.json')
+        self.net = nn.ModuleList(net)
+
+    def forward(self, x):
+        length = len(self.net)
+        input_list = [[], [0], [1], [2], [3], [4], [5], [6, 2], [7], [8], [9], [10], [11, 7], [12], [13], [12], [15], [16], [17], [18, 14], [19], [20], [21], [22], [23, 19], [24], [25], [24], [27], [28], [29], [30, 26], [31], [32], [33], [34], [35, 31], [36], [37], [36], [39], [40], [41], [42, 38], [43], [44], [45], [46], [47, 43], [48], [49], [50]]
+        out = [x for i in range(length)]
+        for i in range(length):
+            # print('{}: {}'.format(i, type(self.net[i])))
+            if i == 0:
+                out[i] = self.net[i](x)
+            elif i == 51 and len(input_list[i]) == 1:
+                out[i] = self.net[i](out[input_list[i][0]])
+            elif len(input_list[i]) == 1:
+                out[i] = self.net[i](out[input_list[i][0]])
+            elif len(input_list[i]) == 2:
+                idx_0 = input_list[i][0]
+                idx_1 = input_list[i][1]
+                input_0 = out[idx_0]
+                input_1 = out[idx_1]
+                out[i] = self.net[i](input_0 + input_1)
+            else:
+                assert 'wrong input list' and False
+            #if i == 3:
+            #    print(out[3])
+        return out[length-1]
+
+
+if __name__ == "__main__":
+    # x = torch.rand(size=(1, 3, 224, 224))
+    with open("/home/lifter/Documents/DL_compiler/BTD_DATA/Glow-2022/resnet18_glow/cat.bin", 'br') as f:
+        bin_data = f.read()
+        np_arr = np.frombuffer(bin_data, dtype=np.float32)
+        print(np_arr.shape)
+        np_arr = np_arr.reshape(3, 224, 224)
+        np_arr = np_arr.reshape((1, 3, 224, 224))
+        x = torch.Tensor(np_arr)
+        print(x.shape)
+
+    time1 = time.time()
+    print('building the model:', end=' ')
+    vgg = MyResNet()
+    time2 = time.time()
+    print('{}s'.format(time2 - time1))
+
+    print('predicting the label:', end=' ')
+    out = vgg(x)
+    time3 = time.time()
+    print('{}s'.format(time3 - time2))
+
+    print(out.size())
+    print(type(out))
+    max_index = np.argmax(out.detach().numpy())
+    print(max_index)
+    # print(out)
+    print(out.detach().numpy()[0, max_index])
+    exit(0)
+
+    # Input to the model
+    vgg.eval()
+
+    batch_size = 1
+    
+    torch_out = vgg(x)
+
+    # Export the model
+    torch.onnx.export(vgg,               # model being run
+                      x,                         # model input (or a tuple for multiple inputs)
+                      "resnet18_glow_rebuild.onnx",   # where to save the model (can be a file or file-like object)
+                      export_params=True,        # store the trained parameter weights inside the model file
+                      opset_version=10,          # the ONNX version to export the model to
+                      # do_constant_folding=True,  # whether to execute constant folding for optimization
+                      input_names = ['input'],   # the model's input names
+                      output_names = ['output'], # the model's output names
+                      )
+    exit(0)
