ce46b8df42922153e3c435e464644108549aa989
Zhibo LIU <monkbai7@163.com>
2022-10-07 20:16:06 +0800

    update

--- decompile_eval.sh
+++ decompile_eval.sh
@@ -27,13 +27,13 @@ echo "BTD home: $CUR_DIR"
 # TMP_DIR=$DATA_DIR/Glow-2020/shufflenet_v2
 # python3 ./shufflenet_glow_decompile.py $TMP_DIR/shufflenet_funcs $TMP_DIR/shufflenet_v2_strip.out $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
 
-# cd $CUR_DIR/evaluation/resnet_glow_2020/
-# TMP_DIR=$DATA_DIR/Glow-2020/resnet18_glow
-# python3 ./resnet18_glow_decompile.py $TMP_DIR/resnet18_glow_ida $TMP_DIR/resnet18_strip.out $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
+cd $CUR_DIR/evaluation/resnet18_glow_2020/
+TMP_DIR=$DATA_DIR/Glow-2020/resnet18_glow
+python3 ./resnet18_glow_decompile.py $TMP_DIR/resnet18_glow_ida $TMP_DIR/resnet18_strip.out $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
 
-# cd $CUR_DIR/evaluation/vgg16_glow_2020/
-# TMP_DIR=$DATA_DIR/Glow-2020/vgg16_glow
-# python3 ./vgg16_glow_decompile.py $TMP_DIR/vgg16_glow_ida $TMP_DIR/vgg16_strip.out $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
+cd $CUR_DIR/evaluation/vgg16_glow_2020/
+TMP_DIR=$DATA_DIR/Glow-2020/vgg16_glow
+python3 ./vgg16_glow_decompile.py $TMP_DIR/vgg16_glow_ida $TMP_DIR/vgg16_strip.out $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
 
 cd $CUR_DIR/evaluation/fasttext_embedding_glow_2020/
 python3 ./embedding_glow_decompile.py $DATA_DIR/embedding/embedding_glow_funcs $DATA_DIR/embedding/embedding_glow $DATA_DIR/embedding/label_glow.txt
