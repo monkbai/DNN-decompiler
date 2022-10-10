ba6492dc7da3d0b5510f407558bbc5ec86872c8d
Zhibo LIU <monkbai7@163.com>
2022-10-10 17:42:20 +0800

    update

--- decompile_eval.sh
+++ decompile_eval.sh
@@ -248,14 +248,13 @@ echo "BTD home: $CUR_DIR"
 
 echo " - Decompiling inception_tvm_v08_O3"
 cd $CUR_DIR/evaluation/inception_tvm_v08_O3/
-TMP_DIR=$DATA_DIR/TVM-v0.7/inceptionv1_tvm_O3
-echo "python3 ./inception_tvm_O3_decompile.py $TMP_DIR/inceptionv1_funcs $TMP_DIR/inceptionv1_tvm_O3_strip $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt"
+TMP_DIR=$DATA_DIR/TVM-v0.8/inceptionv1_tvm_O3
 python3 ./inception_tvm_O3_decompile.py $TMP_DIR/inceptionv1_funcs $TMP_DIR/inceptionv1_tvm_O3_strip $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
 
 echo " - Decompiling mobilenet_tvm_v08_O3"
 cd $CUR_DIR/evaluation/mobilenet_tvm_v08_O3/
 TMP_DIR=$DATA_DIR/TVM-v0.8/mobilenetv2_tvm_O3
-python3 ./mobilenet_tvm_O3_decompile.py $TMP_DIR/mobilenet_funcs $TMP_DIR/mobilenetv2_7_tvm_O3_strip $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
+python3 ./mobilenet_tvm_O3_decompile.py $TMP_DIR/mobilenetv2_funcs $TMP_DIR/mobilenetv2_7_tvm_O3_strip $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
 
 # echo " - Decompiling shufflenet_tvm_v08_O3"
 # cd $CUR_DIR/evaluation/shufflenet_tvm_v08_O3/
@@ -272,19 +271,19 @@ python3 ./mobilenet_tvm_O3_decompile.py $TMP_DIR/mobilenet_funcs $TMP_DIR/mobile
 # TMP_DIR=$DATA_DIR/TVM-v0.8/vgg16_tvm_O3
 # python3 ./vgg16_tvm_O3_decompile.py $TMP_DIR/vgg16_funcs $TMP_DIR/vgg16_tvm_O3_strip $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
 
-echo " - Decompiling fasttext_tvm_v08_O3"
-cd $CUR_DIR/evaluation/fasttext_embedding_tvm_v08_O3/
-python3 ./embedding_tvmO3_decompile.py $DATA_DIR/embedding_extra/embedding_tvm_v08_O3_funcs $DATA_DIR/embedding_extra/embedding_tvm_v08_O3 $DATA_DIR/embedding_extra/label_tvm_v08_O3.txt
+# echo " - Decompiling fasttext_tvm_v08_O3"
+# cd $CUR_DIR/evaluation/fasttext_embedding_tvm_v08_O3/
+# python3 ./embedding_tvmO3_decompile.py $DATA_DIR/embedding_extra/embedding_tvm_v08_O3_funcs $DATA_DIR/embedding_extra/embedding_tvm_v08_O3 $DATA_DIR/embedding_extra/label_tvm_v08_O3.txt
 
 
 # ------- TVM v0.9.dev O0 -------
 #        Decompilation
 #       
 # ---------------------------
-echo " - Decompiling efficientnet_tvm_v09_O0"
-cd $CUR_DIR/evaluation/efficient_tvm_v09_O0/
-TMP_DIR=$DATA_DIR/TVM-v0.9.dev/efficientnet_tvm_O0
-python3 ./efficient_tvm_O0_decompile.py $TMP_DIR/efficientnet_funcs $TMP_DIR/efficientnet_lite4_tvm_O0_strip $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
+# echo " - Decompiling efficientnet_tvm_v09_O0"
+# cd $CUR_DIR/evaluation/efficient_tvm_v09_O0/
+# TMP_DIR=$DATA_DIR/TVM-v0.9.dev/efficientnet_tvm_O0
+# python3 ./efficient_tvm_O0_decompile.py $TMP_DIR/efficientnet_funcs $TMP_DIR/efficientnet_lite4_tvm_O0_strip $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
 
 # echo " - Decompiling inception_tvm_v09_O0"
 # cd $CUR_DIR/evaluation/inception_tvm_v09_O0/
@@ -311,9 +310,9 @@ python3 ./efficient_tvm_O0_decompile.py $TMP_DIR/efficientnet_funcs $TMP_DIR/eff
 # TMP_DIR=$DATA_DIR/TVM-v0.9.dev/vgg16_tvm_O0
 # python3 ./vgg16_tvm_O0_decompile.py $TMP_DIR/vgg16_funcs $TMP_DIR/vgg16_tvm_O0_strip $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
 
-echo " - Decompiling fasttext_tvm_v09_O0"
-cd $CUR_DIR/evaluation/fasttext_embedding_tvm_v09_O0/
-python3 ./embedding_tvmO0_decompile.py $DATA_DIR/embedding_extra/embedding_tvm_v09_O0_funcs $DATA_DIR/embedding_extra/embedding_tvm_v09_O0 $DATA_DIR/embedding_extra/label_tvm_v09_O0.txt
+# echo " - Decompiling fasttext_tvm_v09_O0"
+# cd $CUR_DIR/evaluation/fasttext_embedding_tvm_v09_O0/
+# python3 ./embedding_tvmO0_decompile.py $DATA_DIR/embedding_extra/embedding_tvm_v09_O0_funcs $DATA_DIR/embedding_extra/embedding_tvm_v09_O0 $DATA_DIR/embedding_extra/label_tvm_v09_O0.txt
 
 
 # ------- TVM v0.9.dev O3 -------
@@ -332,26 +331,26 @@ python3 ./embedding_tvmO0_decompile.py $DATA_DIR/embedding_extra/embedding_tvm_v
 echo " - Decompiling mobilenet_tvm_v09_O3"
 cd $CUR_DIR/evaluation/mobilenet_tvm_v09_O3/
 TMP_DIR=$DATA_DIR/TVM-v0.9.dev/mobilenetv2_tvm_v09_O3
-python3 ./mobilenet_tvm_O3_decompile.py $TMP_DIR/mobilenet_funcs $TMP_DIR/mobilenetv2_7_tvm_O3_strip $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
+python3 ./mobilenet_tvm_O3_decompile.py $TMP_DIR/mobilenetv2_funcs $TMP_DIR/mobilenetv2_7_tvm_O3_strip $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
 
 # echo " - Decompiling shufflenet_tvm_v09_O3"
 # cd $CUR_DIR/evaluation/shufflenet_tvm_v09_O3/
 # TMP_DIR=$DATA_DIR/TVM-v0.9.dev/shufflenetv2_tvm_v09_O3
 # python3 ./shufflenet_tvm_O3_decompile.py $TMP_DIR/shufflenetv2_funcs $TMP_DIR/shufflenetv2_tvm_O3_strip $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
 
-echo " - Decompiling resnet18_tvm_v09_O3"
-cd $CUR_DIR/evaluation/resnet18_tvm_v09_O3/
-TMP_DIR=$DATA_DIR/TVM-v0.9.dev/resnet18_tvm_O3
-python3 ./resnet18_tvm_O3_decompile.py $TMP_DIR/resnet18_funcs $TMP_DIR/resnet18_tvm_O3_strip $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
+# echo " - Decompiling resnet18_tvm_v09_O3"
+# cd $CUR_DIR/evaluation/resnet18_tvm_v09_O3/
+# TMP_DIR=$DATA_DIR/TVM-v0.9.dev/resnet18_tvm_O3
+# python3 ./resnet18_tvm_O3_decompile.py $TMP_DIR/resnet18_funcs $TMP_DIR/resnet18_tvm_O3_strip $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
 
 # echo " - Decompiling vgg16_tvm_v09_O3"
 # cd $CUR_DIR/evaluation/vgg16_tvm_v09_O3/
 # TMP_DIR=$DATA_DIR/TVM-v0.9.dev/vgg16_tvm_O3
 # python3 ./vgg16_tvm_O3_decompile.py $TMP_DIR/vgg16_funcs $TMP_DIR/vgg16_tvm_O3_strip $TMP_DIR/cat.bin $TMP_DIR/func_call.log $TMP_DIR/label.txt
 
-echo " - Decompiling fasttext_tvm_v09_O3"
-cd $CUR_DIR/evaluation/fasttext_embedding_tvm_v09_O3/
-python3 ./embedding_tvmO3_decompile.py $DATA_DIR/embedding_extra/embedding_tvm_v09_O3_funcs $DATA_DIR/embedding_extra/embedding_tvm_v09_O3 $DATA_DIR/embedding_extra/label_tvm_v09_O3.txt
+# echo " - Decompiling fasttext_tvm_v09_O3"
+# cd $CUR_DIR/evaluation/fasttext_embedding_tvm_v09_O3/
+# python3 ./embedding_tvmO3_decompile.py $DATA_DIR/embedding_extra/embedding_tvm_v09_O3_funcs $DATA_DIR/embedding_extra/embedding_tvm_v09_O3 $DATA_DIR/embedding_extra/label_tvm_v09_O3.txt
 
 echo "============="
 # =============
