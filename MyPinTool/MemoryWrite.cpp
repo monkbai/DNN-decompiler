4dee337e5be0dbc8e8a3e039e90db90d1e70f6d3
Zhibo LIU <monkbai7@163.com>
2022-10-19 16:48:25 +0800

    clean code

--- MyPinTool/MemoryWrite.cpp
+++ MyPinTool/MemoryWrite.cpp
@@ -1,18 +1,3 @@
-/*
- * Copyright 2002-2019 Intel Corporation.
- * 
- * This software is provided to you as Sample Source Code as defined in the accompanying
- * End User License Agreement for the Intel(R) Software Development Products ("Agreement")
- * section 1.L.
- * 
- * This software and the related documents are provided as is, with no express or implied
- * warranties, other than those that are expressly stated in the License.
- */
-
-/*
- *  This file contains an ISA-portable PIN tool for tracing memory accesses.
- */
-
 #include <stdio.h>
 #include <unordered_map>
 #include <unordered_set>
