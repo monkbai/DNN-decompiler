1d58d7a9c44f17b512721ff13ff9c8f88c3c0818
Zhibo LIU <monkbai7@163.com>
2022-10-27 12:16:13 +0800

    update

--- summarization.sh
+++ summarization.sh
@@ -2,19 +2,23 @@
 CUR_DIR=$PWD
 DATA_DIR=/home/BTD-data
 
-echo "\n======= Table 2 ======="
+echo ""
+echo "======= Table 2 ======="
 python3 ./statistic.py 
 
-echo "\n======= Table 3 ======="
+echo ""
+echo "======= Table 3 ======="
 cd ./operator_inference
 cp AE/* ./
-python3 ./run_accuracy.py 
+python3 ./run_accuracy.py > /dev/null
 cat ./acc.txt
 cd ..
 
-echo "\n======= Table 4 ======="
+echo ""
+echo "======= Table 4 ======="
 python3 ./parameter_accuracy.py
 
-echo "\n======= Table 5 ======="
+echo ""
+echo "======= Table 5 ======="
 ccho "<Pass> means the model is 100% correctly rebuilt."
 python3 ./recompile_correctness.py
