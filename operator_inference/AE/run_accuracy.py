a3f9830bca196e6b4965e770becf23fb3263b8ef
Zhibo LIU <monkbai7@163.com>
2022-10-27 10:06:45 +0800

    update

--- operator_inference/AE/run_accuracy.py
+++ operator_inference/AE/run_accuracy.py
@@ -6,5 +6,5 @@ for (compiler, setting) in [
     ('TVM', 'TVM_v0.7_O0'), ('TVM', 'TVM_v0.8_O0'), ('TVM', 'TVM_v0.9.dev_O0'),
     ('TVM', 'TVM_v0.7_O3'), ('TVM', 'TVM_v0.8_O3'), ('TVM', 'TVM_v0.9.dev_O3')]:
     for model in ['resnet18', 'vgg16', 'inception_v1', 'shufflenet_v2', 'mobilenet', 'efficientnet']:
-        os.system('python accuracy.py --compiler %s  --setting %s --model %s' % (compiler, setting, model))
+        os.system('python3 accuracy.py --compiler %s  --setting %s --model %s' % (compiler, setting, model))
         time.sleep(2)
